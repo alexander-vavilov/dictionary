9ad60bf2c77781f1fb36d34e5134b9c97392c0a7
alexander-vavilov <maksimsraud@gmail.com>
2023-10-15 15:40:57 +0300

    fixed sticky block gaps

--- src/components/SearchResultMain.tsx
+++ src/components/SearchResultMain.tsx
@@ -4,7 +4,7 @@ import { ISearchResult } from '../types'
 
 const SearchResultMain: FC<{ result: ISearchResult }> = ({ result }) => {
   return (
-    <div className='sticky top-0 flex items-center justify-between py-2 backdrop-blur-md dark:bg-neutral-900/70 md:py-4'>
+    <div className='sticky top-0 -mx-4 flex items-center justify-between px-4 py-2 backdrop-blur-md dark:bg-neutral-900/70 md:py-4'>
       <div className='flex flex-col md:gap-4'>
         <h1 className='text-4xl font-medium md:text-5xl'>{result.word}</h1>
         <span className='text-lg text-violet-500 dark:text-violet-400'>
